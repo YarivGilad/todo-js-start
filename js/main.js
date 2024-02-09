8a45a64e6195cd911117c45a1d480403d9d4d9c6
Yair Ken <yairkennn@gmail.com>
2024-02-09 17:15:38 +0200

    First commit

--- js/main.js
+++ js/main.js
@@ -4,52 +4,45 @@
  * @param {HTMLElement} p the parent of the element
  * @returns an HTMLElement
  */
-const $ = (s,p = document)=> p.querySelector(s);
-const genID = ()=> Math.random().toString(36).slice(2,8);
-
+const $ = (s, p = document) => p.querySelector(s);
+const genID = () => Math.random().toString(36).slice(2, 8);
 
 // UI DOM elements
-const form = $('form');
-const todoList = $('ul.todo-list');
-const taskInput = $('#task-input');
+const form = $("form");
+const todoList = $("ul.todo-list");
+const taskInput = $("#task-input");
 
 //global variables
-const todos = [
-    {
-        id: genID(),
-        task:'buy milk',
-        completed:false
-    }
-];
-
+const todos = [];
 
 init();
 
-function init(){
-    form.addEventListener('submit',onFormSubmit);
+function init() {
+  form.addEventListener("submit", onFormSubmit);
 }
 
-function onFormSubmit(event){
-    event.preventDefault();
-    // on new task entered --> create new list item
-    todos.push({
-        id: genID(),
-        task: taskInput.value,
-        completed:false
-    })
-    render();
+function onFormSubmit(event) {
+  event.preventDefault();
+  // on new task entered --> create new list item
+  todos.push({
+    id: genID(),
+    task: taskInput.value,
+    completed: false,
+  });
+  render();
 }
 
-function render(){
-    let markup = '';
-    // loop over todos array
-    for(let todo of todos) {
-        // each item will be translated to li item in the ul...
-        markup += `<li class="todo-item">
-        <input type="checkbox" id="check" ${todo.completed ? 'checked' : ''}>
+function render() {
+  let markup = "";
+  // loop over todos array
+  for (let todo of todos) {
+    // each item will be translated to li item in the ul...
+    markup += `<li class="todo-item">
+        <input type="checkbox" id="check" ${todo.completed ? "checked" : ""}>
         <label for="check">${todo.task}</label>
-        <span>X</span>
-      </li>`
-    }
-    todoList.innerHTML = markup;
+        <span class="delete-btn"><i class="fa-regular fa-trash-can"></i></span>
+      </li>`;
+  }
+  //   console.log(todos);
+  todoList.innerHTML = markup;
 }
