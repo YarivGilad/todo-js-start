a81f68c0c660d70b081f6a4c4ee86975a9c9f00d
Dachi Sapir <dachisapir@gmail.com>
2024-02-11 21:05:40 +0200

    masterpiece completed

---
+++ js/main.js
@@ -0,0 +1,73 @@
+const input = document.querySelector('#input');
+const listContainer = document.querySelector('#listContainer');
+const count = document.querySelector('.count');
+const li = document.querySelector('li');
+count.textContent = listContainer.length;
+console.log(listContainer.length);
+
+const allBtn = document.querySelector('.all');
+const activeBtn = document.querySelector('.active');
+const completedBtn = document.querySelector('.completed');
+
+const add = document.querySelector('.add');
+
+function updateCount(){
+    count.textContent = listContainer.children.length;
+}
+
+add.addEventListener('click', function(event){
+    event.preventDefault();
+
+    let li = document.createElement('li');
+    li.innerHTML = input.value;
+    listContainer.appendChild(li);
+    // count.textContent = listContainer.children.length;
+    updateCount()
+    let span = document.createElement('span');
+    span.innerHTML = '❌';
+    li.appendChild(span);
+
+    li.addEventListener('click', function(e){
+        if(e.target.tagName === 'LI'){
+            e.target.classList.toggle('marked')
+        }else if (e.target.tagName === 'SPAN'){
+            e.target.parentElement.remove()
+            // count.textContent = listContainer.children.length;
+            updateCount()
+        }
+    });
+    activeBtn.addEventListener('click',function(e){
+        if(e.target.classList.contains('active')){
+            const listItems = listContainer.querySelectorAll('li');
+            listItems.forEach(function(li){
+                if(li.classList.contains('marked')){
+                    li.style.display = 'none';
+                }else {
+                    li.style.display = 'list-item';
+                }
+            });
+        }
+    });
+
+    completedBtn.addEventListener('click',function(e){
+        if(e.target.classList.contains('completed')){
+            const listItems = listContainer.querySelectorAll('li');
+            listItems.forEach(function(li){
+                if(!li.classList.contains('marked')){
+                    li.style.display = 'none';
+                }else {
+                    li.style.display = 'list-item';
+                }
+            })
+        }
+    });
+
+    allBtn.addEventListener('click', function(e){
+        if(e.target.classList.contains('all')){
+            const listItems = listContainer.querySelectorAll('li');
+            listItems.forEach(function(li){
+                li.style.display = 'list-item';
+            })
+        }
+    })
+});
